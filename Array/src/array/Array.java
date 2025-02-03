6dbfb05f1b35f7e8fa1634cf6a84d1f069e8101c
prathamesh-1313 <yewaleprathmesh66@gmail.com>
2025-02-03 12:05:06 +0530

    file commited

--- Array/src/array/Array.java
+++ Array/src/array/Array.java
@@ -4,11 +4,11 @@ public class Array {
 
 	public static void main(String[] args) {
 	   
-		int arr[]= {43,67,23,78,90};
+		int arr[]= {43,67,23,78,90,87};
 		
 		int max=Integer.MIN_VALUE;; int secMax=Integer.MIN_VALUE;
 		
-		for(int i=0;i<5;i++) {
+		for(int i=0;i<arr.length;i++) {
 			if(max<arr[i]) {
 				max=arr[i];
 			}
